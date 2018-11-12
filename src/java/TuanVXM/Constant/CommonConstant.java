a457d0e3773966ee3a056b95870639c5b06b7b82
TuanVXM <tuanvxm@gmail.com>
2018-11-12 14:41:39 +0700

    Init webpage

--- src/java/TuanVXM/Constant/CommonConstant.java
+++ src/java/TuanVXM/Constant/CommonConstant.java
@@ -10,13 +10,14 @@ package TuanVXM.Constant;
  * @author TuanVXM
  */
 public class CommonConstant {
+
     public static final String CHARSET_UTF8 = "UTF-8";
-    
+
     public static final String TECH_ONE_PREFIX = "http://www.techone.vn/";
     public static final String TECH_ONE_URL_PATTERN = "http://www.techone.vn/dien-thoai/&page={page}";
-    
+
     public static final String MUA_BAN_URL_PATTERN = "https://muaban.net/do-dien-tu-toan-quoc-l0-c6?kw={search}";
-    
+
     public static final String TECH_ONE_CONFIG_FILE = "D:\\FU-Learning\\PRX\\TuanVXM\\Presentation\\src\\java\\TuanVXM\\Config\\CrawlTechOneConfig.xml";
     public static final String MUA_BAN_CONFIG_FILE = "D:\\FU-Learning\\PRX\\TuanVXM\\Presentation\\src\\java\\TuanVXM\\Config\\CrawlTechOneConfig.xml";
 }
---
+++ src/java/TuanVXM/Constant/UrlConstant.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TuanVXM.Constant;
+
+/**
+ *
+ * @author TuanVXM
+ */
+public class UrlConstant {
+
+    public static String HOME_PAGE_URL = "index.html";
+}
